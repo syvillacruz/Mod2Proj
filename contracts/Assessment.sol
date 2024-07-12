baac7fd87eae6fb526de9a5ccca0b479b1aff6b5
syvillacruz <104409148+syvillacruz@users.noreply.github.com>
2024-07-13 00:16:50 +0800

    Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,60 +1,44 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
-    uint256 public balance;
+    uint256 public contractBalance;
 
-    event Deposit(uint256 amount);
-    event Withdraw(uint256 amount);
+    event Deposit(address indexed user, uint256 amount);
+    event BetPlaced(address indexed user, uint256 amount, uint256 randomNumber, bool isWin);
+    event Win(address indexed user, uint256 amount);
+    event Lose(address indexed user, uint256 amount);
 
-    constructor(uint initBalance) payable {
+    constructor() payable {
         owner = payable(msg.sender);
-        balance = initBalance;
+        contractBalance = 0;
     }
 
-    function getBalance() public view returns(uint256){
-        return balance;
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
     }
 
-    function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
-        balance += _amount;
-
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
-        emit Deposit(_amount);
+    function determineWin(uint256 randomNumber) internal pure returns (bool) {
+        return (randomNumber == 1 || randomNumber == 3 || randomNumber == 5);
     }
 
-    // custom error
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
+    function placeBet() public payable {
+        require(msg.value == 1 ether, "Bet amount must be exactly 1 ETH");
+        uint256 randomNumber = uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty, msg.sender))) % 6 + 1;
+        bool isWin = determineWin(randomNumber);
 
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
-            });
+        if (isWin) {
+            uint256 winAmount = 2 ether;
+            require(contractBalance >= winAmount, "Contract balance is insufficient to pay out winnings");
+            payable(msg.sender).transfer(winAmount);
+            contractBalance -= winAmount;
+            emit Win(msg.sender, winAmount);
+        } else {
+            emit Lose(msg.sender, msg.value);
         }
 
-        // withdraw the given amount
-        balance -= _withdrawAmount;
-
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
-        emit Withdraw(_withdrawAmount);
+        contractBalance += msg.value;
+        emit BetPlaced(msg.sender, msg.value, randomNumber, isWin);
     }
 }
